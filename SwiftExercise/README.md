cc8c725133ff30bcd6d34ee90afe08f65c7acda4
apple <>
2018-12-24 16:43:18 +0800

    delete project

--- SwiftExercise/README.md
+++
@@ -1,3 +0,0 @@
-# SwiftExercise
-
-A description of this package.
--- SwiftExercise/Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-
-import SwiftExerciseTests
-
-var tests = [XCTestCaseEntry]()
-tests += SwiftExerciseTests.allTests()
-XCTMain(tests)
